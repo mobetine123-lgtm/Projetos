50f9c87f62ead73b52ca5690e28de7eb8757d5c3
Marco Obetine <mobetine123@gmail.com>
2026-05-30 21:08:56 -0300

    Compras

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner valorDigitadoPeloUsuario = new Scanner(System.in);
+
+        //definicao de variaveis
+        String continuar = "S";
+        String nomeDoPrato;
+        double precoTotalDaCompra = 0.0;
+        int quantidadeDeIngredientesComprados;
+        int precoIngredientes;
+
+        System.out.println("_________________________________________________");
+        System.out.print("Digite o nome do prato:");
+        nomeDoPrato = valorDigitadoPeloUsuario.nextLine();
+
+        System.out.print("quantos ingredientos foram necessarios para fazer a janta de sexta: ");
+        quantidadeDeIngredientesComprados = valorDigitadoPeloUsuario.nextInt();
+
+        System.out.print("Digite os ingredientes que faltam: ");
+
+         for (int contador = 3; contador <= quantidadeDeIngredientesComprados; );
+        {
+            System.out.println("___________________________________________________");
+            System.out.print("digite o valor do ingrediente R$");
+            double precoTotalIngredientesComprados = valorDigitadoPeloUsuario.nextDouble();
+
+            precoTotalDaCompra = precoTotalIngredientesComprados;
+
+
+
+        }
+
+        valorDigitadoPeloUsuario.close();
+
+    }
+
+
+
+}
